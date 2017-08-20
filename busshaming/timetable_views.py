3fc34695824482d66edf32916224239d800a0dfe
Katie Bell <katie@katharos.id.au>
2017-08-20 11:31:09 +1000

    Add a page to view all the trips for a particular route + day.

---
+++ busshaming/timetable_views.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+from busshaming import fetching
+from busshaming.models import Route, TripDate
+
+DAYS_OF_WEEK = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+
+@ensure_csrf_cookie
+def route_by_date(request, route_id, datestr):
+    date = datetime.strptime(datestr, '%Y%m%d').date()
+    route = get_object_or_404(Route, id=route_id)
+    trip_dates = TripDate.objects.filter(date=date, trip__route=route)
+    data = {'route': route, 'trip_dates': trip_dates}
+    return render(request, 'route_by_date.html', context=data)
+
+
+@ensure_csrf_cookie
+def fetch_timetable(request):
+    fetching.fetch_timetable()
+    return render(request, 'index.html')
--- busshaming/urls.py
+++ busshaming/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from busshaming import views
+from busshaming import timetable_views
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^fetch_timetable', views.fetch_timetable, name='fetch_timetable'),
+    url(r'^timetable/route/(\d+)/date/(\d{8})', timetable_views.route_by_date),
     url(r'^admin/', admin.site.urls),
     url('', include('django_prometheus.urls')),
 ]
