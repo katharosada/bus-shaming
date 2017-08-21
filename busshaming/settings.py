9200b3262753161271fb6651659bab04e7804aa2
Katie Bell <katie@katharos.id.au>
2017-08-21 22:31:26 +1000

    Add secret key as an environment variable.

--- busshaming/settings.py
+++ busshaming/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 # Application definition
 
 INSTALLED_APPS = [
