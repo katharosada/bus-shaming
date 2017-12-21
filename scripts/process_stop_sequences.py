eb76d82f71087b46b9a6960c2230a4f668807288
Katie Bell <katie@katharos.id.au>
2017-12-21 22:58:26 +1100

    Add updating the stop sequences after every timetable update.

--- scripts/process_stop_sequences.py
+++ scripts/process_stop_sequences.py
@@ -1,41 +1,9 @@
-import hashlib
 import django
 
 django.setup()
 
-from django.db import transaction
-from busshaming.models import Route, StopSequence, TripStop
-
-
-def upsert_stop_sequence(trip, sequence):
-    key = ','.join(str(stop_id) for stop_id in sequence)
-    keyhash = hashlib.sha256(key.encode('ascii')).hexdigest()
-    with transaction.atomic():
-        try:
-            stop_sequence = StopSequence.objects.get(sequence_hash=keyhash, route_id=trip.route_id)
-        except StopSequence.DoesNotExist:
-            stop_sequence = StopSequence(sequence_hash=keyhash, stop_sequence=key, route_id=trip.route_id)
-            stop_sequence.length = len(sequence)
-            print('Adding new stop sequence for trip ' + str(trip))
-        stop_sequence.trip_headsign = trip.trip_headsign
-        stop_sequence.trip_short_name = trip.trip_short_name
-        stop_sequence.direction = trip.direction
-        stop_sequence.save()
-        trip.stop_sequence = stop_sequence
-        trip.save()
-
-
-def main():
-    # For each Route, find all the trips and stops
-    for route in Route.objects.all():
-        trips = route.trip_set.filter(stop_sequence__isnull=True).all()
-        for trip in trips:
-            this_sequence = []
-            trip_stops = TripStop.objects.filter(trip=trip).order_by('sequence')
-            for trip_stop in trip_stops:
-                this_sequence.append(trip_stop.stop_id)
-            upsert_stop_sequence(trip, this_sequence)
+from busshaming.data_processing import update_stop_sequences
 
 
 if __name__ == '__main__':
-    main()
+    update_stop_sequences.update_all_stop_sequences()
--- scripts/process_timetable_data.py
+++ scripts/process_timetable_data.py
@@ -3,7 +3,9 @@ import django
 django.setup()
 
 from busshaming.data_processing import process_timetable_data
+from busshaming.data_processing import update_stop_sequences
 
 
 if __name__ == '__main__':
     process_timetable_data.fetch_and_process_timetables()
+    update_stop_sequences.update_all_stop_sequences()
