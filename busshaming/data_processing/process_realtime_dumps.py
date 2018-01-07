db7e2d43aa60e27760b2712629f2b93347679d94
Katie Bell <katie@katharos.id.au>
2018-01-07 20:54:25 +1100

    Better logging and performance improvements for realtime processing.

--- busshaming/data_processing/process_realtime_dumps.py
+++ busshaming/data_processing/process_realtime_dumps.py
@@ -11,21 +11,29 @@ from busshaming.models import Feed, Trip, TripDate, TripStop, RealtimeEntry, Rou
 S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'busshaming-realtime-dumps')
 
 
+DEBUG = False
+global_stats = {}
+
+
 def add_missing_tripdate(feed, realtime_trip):
     gtfs_trip_id = realtime_trip.trip_id
     start_date = realtime_trip.start_date
-    print(f'Adding missing trip date for gtfs id {gtfs_trip_id} on date {start_date}')
+    if DEBUG:
+        print(f'Adding missing trip date for gtfs id {gtfs_trip_id} on date {start_date}')
     date = datetime.strptime(start_date, '%Y%m%d').date()
     try:
         trip = Trip.objects.get(gtfs_trip_id=gtfs_trip_id)
-        print(f'Found trip: {trip}')
+        if DEBUG:
+            print(f'Found trip: {trip}')
         if TripDate.objects.filter(trip=trip, date=date).count() == 0:
             tripdate = TripDate(trip=trip, date=date, added_from_realtime=True)
             tripdate.save()
+            global_stats['missing_tripdates'] += 1
             return tripdate
         return TripDate.objects.get(trip=trip, date=date)
     except Trip.DoesNotExist as e:
-        print(f'Trip with gtfs id {gtfs_trip_id} does not exist!!')
+        if DEBUG:
+            print(f'Trip with gtfs id {gtfs_trip_id} does not exist!!')
         try:
             route = Route.objects.get(feed=feed, gtfs_route_id=realtime_trip.route_id)
             newtrip = Trip(
@@ -38,19 +46,24 @@ def add_missing_tripdate(feed, realtime_trip):
                 bikes_allowed=False,
             )
             newtrip.save()
-            print(f'Added new trip: {newtrip}')
+            global_stats['missing_trips'] += 1
+            if DEBUG:
+                print(f'Added new trip: {newtrip}')
             tripdate = TripDate(trip=newtrip, date=date, added_from_realtime=True)
             tripdate.save()
+            global_stats['missing_tripdates'] += 1
             return tripdate
         except Route.DoesNotExist as e2:
-            print('Route did not exist')
-            print()
+            global_stats['missing_routes'] += 1
+            if DEBUG:
+                print('Route did not exist')
+                print()
             return None
 
 
-def add_missing_trip_stop(trip, trip_update, stop_update, feed_tz):
+def add_missing_trip_stop(trip, trip_update, stop_update, feed_tz, stops):
     stop_id = stop_update.stop_id
-    stop = get_stop(trip.route.feed, stop_id)
+    stop = get_stop(trip.route.feed, stop_id, stops)
 
     if TripStop.objects.filter(trip=trip, stop=stop, sequence=stop_update.stop_sequence).count() != 0:
         return
@@ -67,52 +80,72 @@ def add_missing_trip_stop(trip, trip_update, stop_update, feed_tz):
         timepoint=False
     )
     newtripstop.save()
+    global_stats['missing_tripstops'] += 1
 
 
-def get_stop(feed, stop_id):
+def get_stop(feed, stop_id, stops):
     try:
         stop = Stop.objects.get(feed=feed, gtfs_stop_id=stop_id)
     except Stop.DoesNotExist:
         stop = Stop(feed=feed, gtfs_stop_id=stop_id, name='Unknown', position=None)
         stop.save()
+        stops[stop.gtfs_stop_id] = stop
+        global_stats['missing_stops'] += 1
     return stop
 
 
 def process_trip_update(feed, trip_dates, stops, feed_tz, trip_update, threshold):
+    global_stats['trip_updates_found'] += 1
     trip = trip_update.trip
     key = (trip.trip_id, trip.start_date)
     if key not in trip_dates:
         trip_date = add_missing_tripdate(feed, trip)
         if trip_date is not None:
-            print("COULDN'T FIND IN SCHEDULE: {}".format(key))
-            print(trip)
+            if DEBUG:
+                print("COULDN'T FIND IN SCHEDULE: {}".format(key))
+                print(trip)
     else:
         trip_date = trip_dates[key]
     if trip_date is None:
         return
-    print(f'Upserting realtime entries for tripdate {trip_date.id}')
+    if DEBUG:
+        print(f'Upserting realtime entries for tripdate {trip_date.id}')
     for stop_update in trip_update.stop_time_update:
-        if trip_date.trip.added_from_realtime:
-            add_missing_trip_stop(trip_date.trip, trip_update, stop_update, feed_tz)
+        global_stats['stop_updates_found'] += 1
         if stop_update.arrival.time < threshold:
+            if trip_date.trip.added_from_realtime:
+                add_missing_trip_stop(trip_date.trip, trip_update, stop_update, feed_tz, stops)
             if stop_update.stop_id in stops:
                 stop = stops[stop_update.stop_id]
             else:
-                stop = get_stop(feed, stop_update.stop_id)
+                stop = get_stop(feed, stop_update.stop_id, stops)
             arrival_time = datetime.fromtimestamp(stop_update.arrival.time, feed_tz)
             departure_time = datetime.fromtimestamp(stop_update.departure.time, feed_tz)
             # Upsert RealtimeEntry
             RealtimeEntry.objects.upsert(trip_date.id, stop.id, stop_update.stop_sequence, arrival_time, stop_update.arrival.delay, departure_time, stop_update.departure.delay)
+            global_stats['stop_updates_stored'] += 1
 
 
-def process_dump_contents(feed, contents, trip_dates, stops, feed_tz):
+def process_dump_contents(feed, contents, trip_dates, stops, fetchtime, feed_tz):
+    global global_stats
+    global_stats = {
+        'trip_updates_found': 0,
+        'stop_updates_found': 0,
+        'stop_updates_stored': 0,
+        'missing_trips': 0,
+        'missing_stops': 0,
+        'missing_tripstops': 0,
+        'missing_tripdates': 0,
+        'missing_routes': 0,
+    }
     feed_message = gtfs_realtime_pb2.FeedMessage()
     feed_message.ParseFromString(contents)
-    now = datetime.now(tz=feed_tz)
-    threshold = int((now + timedelta(minutes=3)).timestamp())
+    threshold = int((fetchtime + timedelta(minutes=5)).timestamp())
     for entity in feed_message.entity:
         if entity.HasField('trip_update'):
             process_trip_update(feed, trip_dates, stops, feed_tz, entity.trip_update, threshold)
+    for stat in global_stats:
+        print(f'{stat}: {global_stats[stat]}')
 
 
 def fetch_next_dumps(feed, num_dumps):
@@ -134,7 +167,8 @@ def fetch_next_dumps(feed, num_dumps):
             key = response['Contents'][i]['Key']
             s3 = boto3.resource('s3')
             tmp, tmp_path = tempfile.mkstemp()
-            print(f'Fetching {key}...')
+            if DEBUG:
+                print(f'Fetching {key}...')
             s3.Object(S3_BUCKET_NAME, key).download_file(tmp_path)
             results.append((key, tmp_path))
     else:
@@ -143,27 +177,37 @@ def fetch_next_dumps(feed, num_dumps):
 
 
 def process_next(num_dumps):
-    num_dumps = 1
     feed = Feed.objects.get(slug='nsw-buses')
     cached_dumps = fetch_next_dumps(feed, num_dumps)
     feed_tz = pytz.timezone(feed.timezone)
 
     if len(cached_dumps) != 0:
+        # Prefetch stops
         stops = {}
         for stop in Stop.objects.filter(feed=feed):
             stops[stop.gtfs_stop_id] = stop
 
+        # Prefetch Trip Dates
+        first_key = cached_dumps[0][0]
+        trip_dates = {}
+        datestr = os.path.split(first_key)[1].rstrip('.pb')
+        fetchtime = datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S.%f').replace(tzinfo=timezone.utc)
+        # Assume no bus runs longer than 48h
+        fetchtime = fetchtime.astimezone(feed_tz)
+        start = (fetchtime - timedelta(days=2)).date()
+        end = (fetchtime + timedelta(days=2, minutes=len(cached_dumps))).date()
+        for trip_date in TripDate.objects.filter(date__gte=start, date__lte=end).prefetch_related('trip'):
+            datestr = trip_date.date.strftime('%Y%m%d')
+            trip_dates[(trip_date.trip.gtfs_trip_id, datestr)] = trip_date
+
         for key, tmp_file in cached_dumps:
-            trip_dates = {}
             datestr = os.path.split(key)[1].rstrip('.pb')
             fetchtime = datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S.%f').replace(tzinfo=timezone.utc)
             # Assume no bus runs longer than 48h
             fetchtime = fetchtime.astimezone(feed_tz)
-            start = (fetchtime - timedelta(days=2)).date()
-            end = (fetchtime + timedelta(days=2)).date()
-            for trip_date in TripDate.objects.filter(date__gte=start, date__lte=end).prefetch_related('trip'):
-                datestr = trip_date.date.strftime('%Y%m%d')
-                trip_dates[(trip_date.trip.gtfs_trip_id, datestr)] = trip_date
             with open(tmp_file, 'rb') as f:
                 contents = f.read()
-                process_dump_contents(feed, contents, trip_dates, stops, feed_tz)
+                print(f'Processing {key}')
+                process_dump_contents(feed, contents, trip_dates, stops, fetchtime, feed_tz)
+            feed.last_processed_dump = key
+            feed.save()
