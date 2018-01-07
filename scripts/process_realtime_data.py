4e2f64164b09c481a56920c1b98dd2a38ac02338
Katie Bell <katie@katharos.id.au>
2018-01-07 21:12:11 +1100

    Add profiling code (commented) for profiling the realtime processing.

--- scripts/process_realtime_data.py
+++ scripts/process_realtime_data.py
@@ -1,4 +1,5 @@
 import django
+#import cProfile
 
 django.setup()
 
@@ -6,4 +7,5 @@ from busshaming.data_processing import process_realtime_dumps
 
 
 if __name__ == '__main__':
+    #cProfile.run('process_realtime_dumps.process_next(10)', filename='output.txt')
     process_realtime_dumps.process_next(10)
