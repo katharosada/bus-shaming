451e123d3bb94c14c7c3681b3e57e97cebbeacf7
Katie Bell <katie@katharos.id.au>
2018-01-13 11:52:42 +1100

    Add realtime data to the api.

--- busshaming/api.py
+++ busshaming/api.py
@@ -1,6 +1,14 @@
-from rest_framework import filters, mixins, serializers, viewsets
+from datetime import datetime
 
-from .models import Route, StopSequence, Trip
+from rest_framework import filters, mixins, serializers, viewsets
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from rest_framework_nested.serializers import NestedHyperlinkedModelSerializer
+from rest_framework.permissions import AllowAny
+
+
+from .models import Route, StopSequence, Trip, TripDate, RealtimeEntry
 
 
 class TripSerializer(serializers.HyperlinkedModelSerializer):
@@ -51,3 +59,77 @@ class RouteViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = RouteSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('short_name', 'long_name', 'description')
+
+
+class RealtimeEntrySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RealtimeEntry
+        ordering = ['sequence']
+        fields = ('id', 'stop_id', 'sequence', 'arrival_time', 'arrival_delay', 'departure_time', 'departure_delay')
+
+
+class RealtimeEntryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = RealtimeEntry.objects.all()
+    serializer_class = RealtimeEntrySerializer
+
+
+class TripDateSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {
+        'route_pk': 'trip__route_id',
+    }
+
+    class Meta:
+        model = TripDate
+        fields = ('id', 'trip_id', 'date', 'added_from_realtime')
+
+
+class TripDatePlusSerializer(serializers.HyperlinkedModelSerializer):
+    realtimeentry_set = serializers.SerializerMethodField('get_realtimes')
+
+    def get_realtimes(self, obj):
+        queryset = obj.realtimeentry_set.order_by('sequence')
+        return RealtimeEntrySerializer(queryset, many=True).data
+
+    class Meta:
+        model = TripDate
+        fields = ('id', 'trip_id', 'date', 'added_from_realtime', 'realtimeentry_set')
+        depth = 1
+
+
+class TripDateViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = TripDate.objects.all()
+    serializer_class = TripDateSerializer
+
+
+class RouteDateViewSet(viewsets.ViewSet):
+    serializer_class = TripDateSerializer
+    permission_classes = (AllowAny,)
+
+    def list(self, request, route_pk=None):
+        queryset = TripDate.objects.filter(trip__route_id=route_pk)
+        serializer = TripDateSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, route_pk=None):
+        start_date = datetime.strptime(pk, '%Y-%m-%d').date()
+        queryset = TripDate.objects.filter(trip__route_id=route_pk, date=start_date)
+        serializer = TripDateSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RouteDateTripViewSet(viewsets.ViewSet):
+    serializer_class = TripDateSerializer
+    permission_classes = (AllowAny,)
+
+    def list(self, request, route_pk=None, date_pk=None):
+        start_date = datetime.strptime(date_pk, '%Y-%m-%d').date()
+        queryset = TripDate.objects.filter(trip__route__id=route_pk, date=start_date)
+        serializer = TripDateSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, route_pk=None, date_pk=None):
+        start_date = datetime.strptime(date_pk, '%Y-%m-%d').date()
+        queryset = TripDate.objects.filter(trip__route__id=route_pk, date=start_date, id=pk)
+        tripdate = get_object_or_404(queryset)
+        serializer = TripDatePlusSerializer(tripdate, context={'request': request})
+        return Response(serializer.data)
--- busshaming/urls.py
+++ busshaming/urls.py
@@ -3,22 +3,33 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from busshaming import views
 from busshaming import timetable_views
 
-from .api import RouteViewSet, StopSequenceViewSet, TripViewSet
+from .api import RouteViewSet, StopSequenceViewSet, TripDateViewSet, TripViewSet, RouteDateViewSet, RouteDateTripViewSet, RealtimeEntryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'routes', RouteViewSet)
 router.register(r'trips', TripViewSet)
 router.register(r'sequences', StopSequenceViewSet)
+router.register(r'tripdates', TripDateViewSet)
+router.register(r'realtimeentrys', RealtimeEntryViewSet)
+
+route_router = routers.NestedSimpleRouter(router, 'routes', lookup='route')
+route_router.register('dates', RouteDateViewSet, base_name='dates')
+
+routedate_router = routers.NestedSimpleRouter(route_router, 'dates', lookup='date')
+routedate_router.register('trips', RouteDateTripViewSet, base_name='trips')
+
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^timetable/route/(\d+)/date/(\d{8})', timetable_views.route_by_date),
     url(r'^api/', include(router.urls)),
+    url(r'^api/', include(route_router.urls)),
+    url(r'^api/', include(routedate_router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
