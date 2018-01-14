b541e87beb201a1128e3f65740c8e72cdae3c208
Katie Bell <katie@katharos.id.au>
2018-01-14 12:34:26 +1100

    Support cloning a scheduled trip to an unscheduled duplicate trip.

--- busshaming/models/trip.py
+++ busshaming/models/trip.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db import transaction
 
 
 class Trip(models.Model):
@@ -15,9 +16,32 @@ class Trip(models.Model):
     notes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     added_from_realtime = models.BooleanField(default=False)
+    scheduled = models.BooleanField(default=True)
 
     class Meta:
         unique_together = ('gtfs_trip_id', 'version', 'route')
 
     def __str__(self):
         return f'Trip {self.gtfs_trip_id} v{self.version} (Route {self.route.short_name})'
+
+    def clone_to_unscheduled(self, new_gtfs_trip_id):
+        with transaction.atomic():
+            self.refresh_from_db()
+            new_trip = Trip(
+                    gtfs_trip_id=new_gtfs_trip_id,
+                    version=0,
+                    active=True,
+                    route=self.route,
+                    stop_sequence=self.stop_sequence,
+                    trip_headsign=self.trip_headsign,
+                    trip_short_name=self.trip_short_name,
+                    direction=self.direction,
+                    wheelchair_accessible=self.wheelchair_accessible,
+                    bikes_allowed=self.bikes_allowed,
+                    added_from_realtime=True,
+                    scheduled=False)
+            new_trip.save()
+            for tripstop in self.tripstop_set.all():
+                tripstop.clone_to_new_trip(new_trip)
+        return new_trip
+
--- busshaming/models/trip_stop.py
+++ busshaming/models/trip_stop.py
@@ -14,3 +14,13 @@ class TripStop(models.Model):
 
     def __str__(self):
         return f'Trip {self.trip.gtfs_trip_id} stop {self.sequence} ({self.stop.name})'
+
+    def clone_to_new_trip(self, new_trip):
+        new_tripstop = TripStop(
+                trip=new_trip,
+                stop=self.stop,
+                sequence=self.sequence,
+                arrival_time=self.arrival_time,
+                departure_time=self.departure_time,
+                timepoint=self.timepoint)
+        new_tripstop.save()
